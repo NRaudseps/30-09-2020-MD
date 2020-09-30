bba664d14a13895124adccb3fb4ba1e4241f70c2
nraudseps <nraudseps@gmail.com>
2020-09-30 10:44:37 +0300

    course correction

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
-<?php //declare(strict_types=1);
+<?php declare(strict_types=1);
 
+class File
+{
+    private string $file;
 
-$file = 'numbers.txt';
-$current = file_get_contents($file);
+    public function __construct(string $file)
+    {
+        $this->file = $file;
+    }
 
+    public function getContents(): string
+    {
+        return file_get_contents($this->file);
+    }
+
+    public function putContents(string $current)
+    {
+        file_put_contents($this->file, $current);
+    }
+}
+
+$file = new File('numbers.txt');
+$current = $file->getContents();
 $number = rand(1, 1000);
 
 $current .= PHP_EOL . $number;
 
 $numArr = explode(PHP_EOL, $current);
 
-file_put_contents($file, $current);
+$file->putContents($current);
 echo 'Random Number Generated: ' . $number . PHP_EOL;
 echo 'Average of Numbers: ' . number_format(array_sum($numArr)/count($numArr), 2) . PHP_EOL;
