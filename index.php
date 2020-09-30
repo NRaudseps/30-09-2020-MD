59a105b73313d22fa178c024a19f08aa2f8f057a
nraudseps <nraudseps@gmail.com>
2020-09-30 10:31:19 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+
+$file = 'numbers.txt';
+$current = file_get_contents($file);
+
+$number = rand(1, 1000);
+
+$current .= PHP_EOL . $number;
+
+$numArr = explode(PHP_EOL, $current);
+
+file_put_contents($file, $current);
+echo 'Random Number Generated: ' . $number . PHP_EOL;
+echo 'Average of Numbers: ' . array_sum($numArr)/count($numArr) . PHP_EOL;
